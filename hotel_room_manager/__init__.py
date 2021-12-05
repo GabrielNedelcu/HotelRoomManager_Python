c82e3ed175bb9209911b3838ee72dd35cd4a92ff
gabriel.nedelcu <gabriel.nedelcu2000@gmail.com>
2021-12-04 18:38:29 -0800

    Fixed small error regarding routes

--- hotel_room_manager/__init__.py
+++ hotel_room_manager/__init__.py
@@ -4,7 +4,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'bf2bd361-3c2f-4676-97e1-cd85ea71d9de'
 
-    from .routes import views
+    from .routes import routes
 
     app.register_blueprint(routes, url_prefix = '/')
     return app
--- hotel_room_manager/routes.py
+++ hotel_room_manager/routes.py
@@ -1,41 +1,41 @@
 from flask import Blueprint
 from flask import render_template
 
-views = Blueprint('views', __name__)
+routes = Blueprint('routes', __name__)
 
-@views.route('/')
-@views.route('/home')
+@routes.route('/')
+@routes.route('/home')
 def home():
     return render_template("home.html")
 
-@views.route('/view-rooms')
+@routes.route('/view-rooms')
 def view_rooms():
     return render_template("all_rooms.html")
 
-@views.route('/view-clients')
+@routes.route('/view-clients')
 def view_clients():
     return render_template("all_clients.html")
 
-@views.route('/view-reservations')
+@routes.route('/view-reservations')
 def view_reservations():
     return render_template("all_reservations.html")
 
-@views.route('/client-profile')
+@routes.route('/client-profile')
 def view_client_profile():
     return render_template("client_profile.html")
 
-@views.route('/room-profile')
+@routes.route('/room-profile')
 def view_room_profile():
     return render_template("room_profile.html")
 
-@views.route('/add-client')
+@routes.route('/add-client')
 def add_client():
     return render_template("insert_client.html")
 
-@views.route('/add-room')
+@routes.route('/add-room')
 def add_room():
     return render_template("insert_room.html")
 
-@views.route('/make-reservation')
+@routes.route('/make-reservation')
 def add_reservation():
     return render_template("make_reservation.html")
